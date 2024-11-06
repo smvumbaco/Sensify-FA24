706071cd41bcc436696ff0cd2a19675ac9028445
sammoy21 <moy.s@northeastern.edu>
2024-11-06 16:04:57 -0500

    bms firmware initial commit

---
+++ production/src/BMS/src/i2c_test.ino
@@ -0,0 +1,11 @@
+#include <driver/i2c.h>
+
+#define MAX17320_ADDR 0x16
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
